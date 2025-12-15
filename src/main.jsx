5bad52fd47bf742be4d3269f16a8c843cdbe0a56
23Jaideep <153830261+23Jaideep@users.noreply.github.com>
2025-12-15 18:30:44 +0530

    Fix import statement for CoinProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './global.css';
 import './index.css'
 import App from './App.jsx'
-import { CoinProvider } from './context/CoinContext.jsx';
+// import { CoinProvider } from './context/CoinContext.jsx';
+import { CoinProvider } from './context/CoinContext';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
