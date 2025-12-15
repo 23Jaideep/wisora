d537b15ead6f8cf12580e16b502d0d4aa0c34365
23Jaideep <153830261+23Jaideep@users.noreply.github.com>
2025-12-15 18:45:53 +0530

    Reorganize CoinHUD placement within Router
    Moved CoinHUD component inside BrowserRouter for proper context.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WelcomePage from './components/welcomePage';
-import Home from './components/home';
-import Studying from './components/Studying';
-import BookSection from './components/BookSection';
-import CoinHUD from './components/CoinHUD';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WelcomePage from "./components/welcomePage";
+import Home from "./components/home";
+import Studying from "./components/Studying";
+import BookSection from "./components/BookSection";
+import CoinHUD from "./components/CoinHUD";
 
 function App() {
   return (
-    <>
-      <CoinHUD />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/BookSection" element={<BookSection />} />
-          <Route path="/Studying" element={<Studying />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <CoinHUD />   {/* ✅ Now safely inside Provider */}
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/BookSection" element={<BookSection />} />
+        <Route path="/Studying" element={<Studying />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
