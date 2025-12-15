d98ebf9296fffa3e7f84a86a93eec9193e560343
23Jaideep <153830261+23Jaideep@users.noreply.github.com>
2025-12-15 18:18:58 +0530

    Fix import case for CoinContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './global.css';
 import './index.css'
 import App from './App.jsx'
-import { CoinProvider } from './context/coinContext.jsx';
+import { CoinProvider } from './context/CoinContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
