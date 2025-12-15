698ce899c4380d966681923c3c0992468f942719
23Jaideep <153830261+23Jaideep@users.noreply.github.com>
2025-12-15 18:35:10 +0530

    Fix import paths and update JSX structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WelcomePage from './components/welcomePage.jsx';
-import Home from './components/home.jsx';
-import Studying from './components/Studying.jsx';
-import BookSection from './components/BookSection.jsx'; 
-import CoinHUD from './components/CoinHUD.jsx';
+import WelcomePage from './components/welcomePage';
+import Home from './components/home';
+import Studying from './components/Studying';
+import BookSection from './components/BookSection';
+import CoinHUD from './components/CoinHUD';
+
 function App() {
   return (
     <>
-    <CoinHUD/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/BookSection" element={<BookSection />} />
-        <Route path = "/Studying" element = {<Studying/>}></Route>
-      </Routes>
-    </BrowserRouter>
+      <CoinHUD />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/BookSection" element={<BookSection />} />
+          <Route path="/Studying" element={<Studying />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
