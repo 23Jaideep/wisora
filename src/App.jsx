d86c41215ba06644b23e768cb155fd57134548c2
23Jaideep <153830261+23Jaideep@users.noreply.github.com>
2025-12-15 18:56:25 +0530

    Lazy-load Studying component with Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
 import WelcomePage from "./components/welcomePage";
 import Home from "./components/home";
-import Studying from "./components/Studying";
 import BookSection from "./components/BookSection";
 import CoinHUD from "./components/CoinHUD";
+
+// Lazy-loaded component
+const Studying = lazy(() => import("./components/Studying"));
 
 function App() {
   return (
     <BrowserRouter>
-      <CoinHUD />   {/* ✅ Now safely inside Provider */}
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/BookSection" element={<BookSection />} />
-        <Route path="/Studying" element={<Studying />} />
-      </Routes>
+      <CoinHUD />
+
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/BookSection" element={<BookSection />} />
+          <Route path="/Studying" element={<Studying />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
